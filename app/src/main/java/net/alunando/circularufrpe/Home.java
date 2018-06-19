57185a43bd6b7ad114c932a7b5f2db6d537316c4
White Dark <whiteofdark@outlook.com>
2018-06-19 15:42:20 -0300

    Testando

---
+++ app/src/main/java/net/alunando/circularufrpe/Home.java
@@ -0,0 +1,16 @@
+package net.alunando.circularufrpe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
